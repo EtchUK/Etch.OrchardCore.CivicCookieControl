1d8f3bb19d83942aec2f9e52633f609b6eb58186
Peter Keating <peter@moov2.com>
2020-03-08 22:26:17 +0000

    Create module with boilerplate template

---
+++ Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Author = "Etch UK Ltd.",
+    Category = "Content",
+    Description = "Add Cookie Control by CIVIC."
+    Name = "CIVIC Cookie Control",
+    Version = "0.0.1",
+    Website = "https://etchuk.com"
+)]
