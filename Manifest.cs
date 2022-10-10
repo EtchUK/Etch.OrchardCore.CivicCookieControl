38d54965b03459fd3d1c12ccdcb8cded9531fc61
Peter Keating <pete.keating@etchuk.com>
2022-10-10 17:59:14 +0100

    v1.3.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Add Cookie Control by CIVIC.",
     Name = "CIVIC Cookie Control",
-    Version = "1.2.0",
+    Version = "1.3.0",
     Website = "https://etchuk.com",
     Dependencies = new[]
     {
